3273bb76f287e5bf544ac2f4ce9c5997d9c19f77
Sonya Pinero <sonyapinero@gmail.com>
2016-07-30 05:04:49 -0400

    lots of tests

--- js/links.js
+++ js/links.js
@@ -1,39 +1,7 @@
-window.onload = function() {
-  var isPrint = false;
-  var tween;
-  var cArray = ['#CC', '#CO', '#CN', '#CT', '#CA', '#CC1', '#CT1'];
-  var pArray = ['#PC', '#PO', '#PN', '#PT', '#PA', '#PC1', '#PT1'];
+window.onload = function(){
+  console.log('hit')
+  var contact = new SVGMorpheus('#contact-animate');
 
-  $( "#contact-cursive" ).hover(
-  function() {
-    toPrint();
-
-    if (!isPrint){
-      toPrint();
-    } else {
-      toCursive();
-    }
-
-  }, function() {
-    //nothing
-  }
-);
-
-function toPrint(){
-  isPrint = true;
-    for(var i = 0; i < cArray.length; i++){
-      tween = KUTE.to(cArray[i], { path: pArray[i]}).start();
-    }
-};
-
-function toCursive(){
-  isPrint = false;
-    for(var i = 0; i < cArray.length; i++){
-      tween = KUTE.to(pArray[i], { path: cArray[i]}).start();
-    }
-  };
-  
-   
-};
-
+  contact.to('print-text');
+}
 
