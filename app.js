9651a74f39d0227fa2ec490de67b8b6dee92b619
Sonya Pinero <sonyapinero@gmail.com>
2016-08-21 19:08:45 -0400

    trying to get kute.js to work

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ import KUTE from 'kute.js'
 
 window.onload = () => {
   console.log('hitting app')
+  let everyday = document.getElementById('everyday')
+  let everydayEl = document.getElementById('everyday').contentDocument.getElementById('everydayCursive');
+  let everydayElLine = document.getElementById('everyday').contentDocument.getElementById('everydayCursiveLine');
 
+  everyday.addEventListener("mouseover", 
+    console.log('hit')
+    let tween = KUTE.to(everdayEl, {path : everydayCursiveLine}).start();
+
+    );
+
+  
 
 }
 
