f0754170aa4962a916687223a0a8a9e11e08db88
Sonya Pinero <sonyapinero@gmail.com>
2016-08-21 21:14:20 -0400

    tweening

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ window.onload = () => {
   let everyday = document.getElementById('everyday')
   let everydayEl = document.getElementById('everyday').contentDocument.getElementById('everydayCursive');
   let everydayElLine = document.getElementById('everyday').contentDocument.getElementById('everydayCursiveLine');
+  let tween = KUTE.to(everydayEl, {path : everydayElLine }, {showMorphInfo: true})
 
-  everyday.addEventListener("mouseover", 
-    console.log('hit')
-    let tween = KUTE.to(everdayEl, {path : everydayCursiveLine}).start();
+  everyday.addEventListener("mouseover", () => {
+    tween.start();
 
-    );
+  });
 
   
 
